a026b572c778423c6f7b49ce1d68432ec3e5c4c3
Ubuntu <ubuntu@ip-172-31-11-198.us-west-2.compute.internal>
2013-07-27 04:06:27 +0000

    init

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+var fs = require('fs');
+
+var buffer = new Buffer(fs.readFileSync("index.html"), 'utf-8');
+
+app.get('/', function(request, response) {
+  response.send(buffer.toString());
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
